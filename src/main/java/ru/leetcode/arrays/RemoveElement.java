2906da5696e6b3d301c12dc17410a068cce687e1
Pavel Glukhov <pvglukhov@gmail.com>
2025-12-04 01:25:31 +0300

    [master] remove elements

--- src/main/java/ru/leetcode/arrays/RemoveElement.java
+++ src/main/java/ru/leetcode/arrays/RemoveElement.java
@@ -2,29 +2,13 @@ package ru.leetcode.arrays;
 
 public class RemoveElement {
     public int removeElement(int[] nums, int val) {
-        if (nums.length == 0) {
-            return 0;
-        }
-
-        int lastIndex = nums.length - 1;
-        int index = 0;
-        int count = 0;
-        do {
-            if (nums[index] == val) {
-                while (index < lastIndex && nums[index] == nums[lastIndex]) {
-                    --lastIndex;
-                }
-                if (index < lastIndex) {
-                    nums[index] = nums[lastIndex];
-                    --lastIndex;
-                    ++index;
-                    ++count;
-                }
-            } else {
-                ++index;
-                ++count;
+        int i = 0;
+        for (int j = 0; j < nums.length; j++) {
+            if (nums[j] != val) {
+                nums[i] = nums[j];
+                i++;
             }
-        } while (index < lastIndex);
-        return count;
+        }
+        return i;
     }
 }
