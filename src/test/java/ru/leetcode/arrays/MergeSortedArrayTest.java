00d4368f1fd32e403a30e28554f3df3976f8a28a
Pavel Glukhov <pvglukhov@gmail.com>
2025-12-04 23:20:10 +0300

    [master] добавлена ещё одна задача

---
+++ src/test/java/ru/leetcode/arrays/MergeSortedArrayTest.java
@@ -0,0 +1,28 @@
+package ru.leetcode.arrays;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MergeSortedArrayTest {
+
+    @Test
+    void mergeCase1Test() {
+    }
+
+    @Test
+    void mergeCase2Test() {
+    }
+
+    @Test
+    void mergeCase3Test() {
+    }
+
+    @Test
+    void mergeCase4Test() {
+    }
+
+    @Test
+    void mergeCase5Test() {
+    }
+}
--- src/test/java/ru/leetcode/arrays/PlusOneTest.java
+++ src/test/java/ru/leetcode/arrays/PlusOneTest.java
@@ -4,27 +4,56 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class PlusOneTest {
+    /**
+     Example 1:
+
+     Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+     Output: [1,2,2,3,5,6]
+     Explanation: The arrays we are merging are [1,2,3] and [2,5,6].
+     The result of the merge is [1,2,2,3,5,6] with the underlined elements coming from nums1.
+
+     Example 2:
+
+     Input: nums1 = [1], m = 1, nums2 = [], n = 0
+     Output: [1]
+     Explanation: The arrays we are merging are [1] and [].
+     The result of the merge is [1].
+
+     Example 3:
+
+     Input: nums1 = [0], m = 0, nums2 = [1], n = 1
+     Output: [1]
+     Explanation: The arrays we are merging are [] and [1].
+     The result of the merge is [1].
+     Note that because m = 0, there are no elements in nums1. The 0 is only there to ensure the merge result can fit in nums1.
+     */
+
 
     @Test
     void plusOneCase1() {
-        int[] nums = new int[]{1,2,3}; // Input array
-        PlusOne p = new PlusOne();
-        Assertions.assertArrayEquals(new int[] {1,2,4}, p.plusOne(nums));
+        int[] nums1 = new int[]{1,2,3,0,0,0}; // Input array
+        int m = 3;
+        int[] nums2 = new int[]{2,5,6}; // Input array
+        int n = 3;
+        int[] output = new int [] {1,2,2,3,5,6};
     }
 
     @Test
     void plusOneCase2() {
-        int[] nums = new int[]{4,3,2,1}; // Input array
-        PlusOne p = new PlusOne();
-        Assertions.assertArrayEquals(new int[] {4,3,2,2}, p.plusOne(nums));
+        int[] nums1 = new int[]{}; // Input array
+        int m = 1;
+        int[] nums2 = new int[]{}; // Input array
+        int n = 0;
+        int[] output = new int [] {1};
     }
 
 
     @Test
     void plusOneCase3() {
-        int[] nums = new int[]{9}; // Input array
-        PlusOne p = new PlusOne();
-        int [] res = p.plusOne(nums);
-        Assertions.assertArrayEquals(new int[] {1, 0}, res);
+        int[] nums1 = new int[]{0}; // Input array
+        int m = 0;
+        int[] nums2 = new int[]{1}; // Input array
+        int n = 1;
+        int[] output = new int [] {1};
     }
 }
