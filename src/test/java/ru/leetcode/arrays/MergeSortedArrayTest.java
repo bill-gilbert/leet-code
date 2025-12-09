98a22b5f06bc79e8fbfa715e5476fc235bf72cab
Pavel Glukhov <pvglukhov@gmail.com>
2025-12-09 22:42:45 +0300

    [master] добавлена ещё одна задача

--- src/test/java/ru/leetcode/arrays/MergeSortedArrayTest.java
+++ src/test/java/ru/leetcode/arrays/MergeSortedArrayTest.java
@@ -8,21 +8,40 @@ class MergeSortedArrayTest {
 
     @Test
     void mergeCase1Test() {
+        int[] nums1 = new int[]{1, 2, 3, 0, 0, 0};
+        int m = 3;
+        int[] nums2 = new int[]{2, 5, 6};
+        int n = 3;
+        int[] result = new int[]{1, 2, 2, 3, 5, 6};
+
+        MergeSortedArray mergeSortedArray = new MergeSortedArray();
+        mergeSortedArray.merge(nums1, m, nums2, n);
+        assertArrayEquals(result, nums1);
     }
 
     @Test
     void mergeCase2Test() {
+        int[] nums1 = new int[]{1};
+        int m = 1;
+        int[] nums2 = new int[]{};
+        int n = 0;
+        int[] result = new int[]{1};
+
+        MergeSortedArray mergeSortedArray = new MergeSortedArray();
+        mergeSortedArray.merge(nums1, m, nums2, n);
+        assertArrayEquals(result, nums1);
     }
 
     @Test
     void mergeCase3Test() {
+        int[] nums1 = new int[]{0};
+        int m = 0;
+        int[] nums2 = new int[]{1};
+        int n = 1;
+        int[] result = new int[]{1};
+        MergeSortedArray mergeSortedArray = new MergeSortedArray();
+        mergeSortedArray.merge(nums1, m, nums2, n);
+        assertArrayEquals(result, nums1);
     }
 
-    @Test
-    void mergeCase4Test() {
-    }
-
-    @Test
-    void mergeCase5Test() {
-    }
 }
